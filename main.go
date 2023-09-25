dbad3d0074bc79c295220fc82ffd64ce5f29e443
Altamash Ali <14983914+Altamashattari@users.noreply.github.com>
2023-09-25 08:33:00 +0530

    contact page

--- main.go
+++ main.go
@@ -5,12 +5,29 @@ import (
 	"net/http"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
+func homehandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	fmt.Fprint(w, "<h1>Welcome to picbucket</h1>")
 }
 
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, `
+		<h1>Contact Page</h1>
+		<p>
+			You can get in touch with me at 
+				<a 
+					href="mailto:altamashattari786@gmail.com"
+				>
+				altamashattari786@gmail.com
+			</a>
+		</p>
+	`)
+}
+
 func main() {
-	http.HandleFunc("/", handlerFunc)
+	http.HandleFunc("/", homehandler)
+	http.HandleFunc("/contact", contactHandler)
 	fmt.Println("Starting the server at port :8080")
 	http.ListenAndServe(":8080", nil)
 }
