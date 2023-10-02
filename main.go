71bdf23877cb364f26c481443761dca93fdeacef
Altamash Ali <14983914+Altamashattari@users.noreply.github.com>
2023-10-02 10:54:13 +0530

    usinng Must function

--- main.go
+++ main.go
@@ -10,6 +10,25 @@ import (
 	"github.com/msa-ali/picbucket/views"
 )
 
+func main() {
+	r := chi.NewRouter()
+
+	tpl := views.Must(views.Parse(filepath.Join("templates", "home.gohtml")))
+	r.Get("/", controllers.StaticHandler(tpl))
+
+	tpl = views.Must(views.Parse(filepath.Join("templates", "contact.gohtml")))
+	r.Get("/contact", controllers.StaticHandler(tpl))
+
+	tpl = views.Must(views.Parse(filepath.Join("templates", "faq.gohtml")))
+	r.Get("/faq", controllers.StaticHandler(tpl))
+
+	tpl = views.Must(views.Parse(filepath.Join("templates", "notfound.gohtml")))
+	r.NotFound(controllers.StaticHandler(tpl))
+
+	fmt.Println("Starting the server at port :8080")
+	http.ListenAndServe(":8080", r)
+}
+
 // func notFoundhandler(w http.ResponseWriter, r *http.Request) {
 // 	// 1
 // 	// w.Header().Set("Content-Type", "text/html; charset=utf-8")
@@ -21,32 +40,3 @@ import (
 // 	tplPath := filepath.Join("templates", "notfound.gohtml")
 // 	executeTemplate(w, tplPath)
 // }
-
-func main() {
-	r := chi.NewRouter()
-	tpl, err := views.Parse(filepath.Join("templates", "home.gohtml"))
-	if err != nil {
-		panic(err)
-	}
-	r.Get("/", controllers.StaticHandler(tpl))
-
-	tpl, err = views.Parse(filepath.Join("templates", "contact.gohtml"))
-	if err != nil {
-		panic(err)
-	}
-	r.Get("/contact", controllers.StaticHandler(tpl))
-
-	tpl, err = views.Parse(filepath.Join("templates", "faq.gohtml"))
-	if err != nil {
-		panic(err)
-	}
-	r.Get("/faq", controllers.StaticHandler(tpl))
-
-	tpl, err = views.Parse(filepath.Join("templates", "notfound.gohtml"))
-	if err != nil {
-		panic(err)
-	}
-	r.NotFound(controllers.StaticHandler(tpl))
-	fmt.Println("Starting the server at port :8080")
-	http.ListenAndServe(":8080", r)
-}
