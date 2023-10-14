de9818d4fe7cd1725cb2310e37c51ce9d9b959a9
Altamash Ali <14983914+Altamashattari@users.noreply.github.com>
2023-10-14 07:16:12 +0530

    added the users.Create to handle signup

--- controllers/users.go
+++ controllers/users.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"fmt"
 	"net/http"
 )
 
@@ -12,4 +13,21 @@ type Users struct {
 
 func (u Users) New(w http.ResponseWriter, r *http.Request) {
 	u.Templates.New.Execute(w, nil)
+}
+
+// note: gorilla/schema is 3rd party lib which can be used to handle complex forms
+func (u Users) Create(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	email := r.PostForm.Get("email")
+	pass := r.PostForm.Get("password")
+	if email == "" || pass == "" {
+		http.Error(w, "invalid form values", http.StatusBadRequest)
+		return
+	}
+	fmt.Println(email, pass)
+	fmt.Fprint(w, "User signed up!!")
 }
